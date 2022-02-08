49c582378d1f42173e3d42133b22b5229425879f
sarah loghmani <sarah.loghmani80@gmail.com>
2022-02-08 18:10:09 +0330

    1

---
+++ js2-1.js
@@ -0,0 +1,36 @@
+// ! this
+
+//?function
+//*declarations functions
+
+//*declaration function nested in declaration function
+
+//*arrow function nested in declaration function
+
+// *arrow function
+
+//* declaration function nested in arrow function =>window
+
+// *arrow function nested in arrow function =>window
+
+// ?method => function in object
+
+
+const test = {
+
+  //*declaration function ***********************************
+  //first way
+
+  //second way
+
+  //*declaration function nested in declaration function
+
+  //*arrow function nested in declaration function
+
+  //* arrow function***************************************
+
+  //* declaration function nested in arrow function
+
+  //* arrow function nested in arrow function
+};
+
